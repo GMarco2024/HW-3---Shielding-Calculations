21d37d305c1be7bb81f69220db776d273943b445
GMarco2024 <mgonzalez11@hawk.iit.edu>
2024-02-22 15:05:40 -0800

    Changes were made.

--- HW 3 - Shielding Calculations/ContentView.swift
+++ HW 3 - Shielding Calculations/ContentView.swift
@@ -11,8 +11,8 @@ import SwiftUI
 struct ContentView: View {
     
     @State var NumberOfPathsString = "1"
-    @State var BeamOffTheGroundString = ""
-    @State var EnergyLossString = ""
+    @State var BeamOffTheGroundString = "4m"
+    @State var EnergyLossString = ".10"
     @State var NeutronAbsorptionString = ""
     @State var NeutronEscapeeString = ""
     
@@ -20,8 +20,6 @@ struct ContentView: View {
         
         ScrollView{
             VStack {
-                
-                
                 
                 Text("HW 3 Shielding Calculations of Neutrons")
                     .underline(true, color: .black)
@@ -39,32 +37,40 @@ struct ContentView: View {
                     .fontWeight(.regular)
                 
                 TextField("number of Paths", text: $NumberOfPathsString)
+                    .frame(maxWidth: 350)
                     .padding()
                 
-                Text("Height of Beam Off Ground")
+                Text("Height of Beam Off Ground (In Meters)")
                     .font(.headline)
                     .fontWeight(.regular)
                 
                 TextField("Height", text: $BeamOffTheGroundString)
+                    .frame(maxWidth: 350)
                     .padding()
                 
-                Text("Energy Loss")
+                Text("Energy Loss (Percentage)")
                     .font(.headline)
                     .fontWeight(.regular)
                 
                 TextField("Loss of Energy", text: $EnergyLossString)
+                    .frame(maxWidth: 350)
+                    .padding()
                 
                 Text("Absorbed Neutrons")
                     .font(.headline)
                     .fontWeight(.regular)
                 
                 TextField("Number of Nuetrons Absorbed by Wall", text: $NeutronAbsorptionString)
+                    .frame(maxWidth: 350)
+                    .padding()
                 
                 Text("Escaped Neutrons")
                     .font(.headline)
                     .fontWeight(.regular)
                 
                 TextField("Number of Neutrons Escaped", text: $NeutronEscapeeString)
+                    .frame(maxWidth: 350)
+                    .padding()
                 
             }
             .padding()
